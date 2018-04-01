feadcb98efb0a71ba792bc6226aa88a798958bb6
luoruixuan <1500012842@pku.edu.cn>
2018-04-01 14:59:54 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import visitor.*;
+import syntaxtree.*;
+import java.io.FileInputStream;
+
+
+class MyVisitor extends DepthFirstVisitor {
+	public void visit(VarDeclaration n) {
+		Identifier id = (Identifier)n.f1;
+		System.out.println("VarName:"+id.f0.toString());
+		n.f0.accept(this);
+		n.f1.accept(this);
+		n.f2.accept(this);
+	}
+}
+
+public class Main{
+	public static void main(String args[]) {
+		try {
+			FileInputStream in = new FileInputStream(args[0]);
+			Node root = new MiniJavaParser(in).Goal();
+			root.accept(new MyVisitor());
+		}catch (ParseException e){
+			e.printStackTrace();
+		}catch (TokenMgrError e){
+			e.printStackTrace();
+		}catch (Exception e){
+			e.printStackTrace();
+		}	
+	}
+}
