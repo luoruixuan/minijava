14d7c56c133f7bab6ade2d2a7757dcb2109d97a7
luoruixuan <1500012842@pku.edu.cn>
2018-04-02 17:01:35 +0800

    typecheck start
    Signed-off-by: luoruixuan <1500012842@pku.edu.cn>

--- src/symboltable/SymbolTable.java
+++ src/symboltable/SymbolTable.java
@@ -5,11 +5,28 @@ import java.util.*;
 public class SymbolTable extends Symbol{
 	public Hashtable<String, ClassSymbol> classes;
 	String mainclass;
+	
+	// For type check
+	public String presentClass, presentMethod;
+	
 	public SymbolTable() {
 		ClassSymbol O = new ClassSymbol("Object", null);
 		mainclass = null;
 		classes = new Hashtable<String, ClassSymbol>();
 		classes.put("Object", O);
+	}
+	public void SetStatus(String C, String M) {
+		presentClass = C;
+		presentMethod = M;
+	}
+	public void SetClass(String C) {
+		presentClass = C;
+	}
+	public void SetMethod(String M) {
+		presentMethod = M;
+	}
+	public boolean isAnsistor(String C1, String C2) {
+		return true;
 	}
 }
 
